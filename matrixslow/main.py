521f9c46a483318bec32627f3a662412152a115c
chenzhen <chenzhen@360.cn>
2019-07-09 18:33:48 +0800

    LR works fine

--- matrixslow/main.py
+++ matrixslow/main.py
@@ -6,44 +6,117 @@ from ops.ops import Add, MatMul, Logistic
 from ops.loss import LogLoss
 from optimizer.optimizer import GradientDescent
 from core.graph import default_graph
-from util.test_data_util import get_data
 import numpy as np
+import matplotlib
+import random
+from sklearn.metrics import accuracy_score
 
+matplotlib.use('TkAgg')
 sys.path.append('.')
 
 
+def plot_data(data_x, data_y, weights=None, bias=None):
+    '''
+    绘制数据节点和线性模型，只绘制二维
+    '''
+    import matplotlib.pyplot as plt
+    assert len(data_x) == len(data_y)
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(data_x.shape[0]):
+        if int(data_y[i]) == 1:
+            xcord1.append(data_x[i, 0])
+            ycord1.append(data_x[i, 1])
+        else:
+            xcord2.append(data_x[i, 0])
+            ycord2.append(data_x[i, 1])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    if weights is not None and bias is not None:
+        x = np.arange(-1.0, 1.0, 0.1)
+        weights = np.array(weights)
+        bias = np.array(bias)
+        y = (-weights[0][0] * x - bias[0][0]) / weights[1][0]
+        ax.plot(x, y)
+    plt.show()
+
+
+def random_gen_dateset(feature_num, sample_num, test_radio=0.3, seed=41):
+    '''
+    生成二分类样本
+    '''
+    random.seed(seed)
+    rand_bias = np.mat(np.random.uniform(-0.1, 0.1, (sample_num, 1)))
+    rand_weights = np.mat(np.random.uniform(-1, 1, (feature_num, 1)))
+    data_x = np.mat(np.random.uniform(-1, 1, (sample_num, feature_num)))
+    data_y = (data_x * rand_weights) + rand_bias
+    data_y = np.where(data_y > 0, 1, 0)
+    train_size = int(sample_num * (1 - test_radio))
+
+    return (data_x[:train_size, :], data_y[:train_size, :], data_x[train_size:, :], data_y[train_size:, :])
+
+
 def build_model(feature_num):
+    '''
+    构建LR计算图模型
+    '''
     x = Variable((1, feature_num), init=False, trainable=False)
     w = Variable((feature_num, 1), init=True, trainable=True)
     b = Variable((1, 1), init=True, trainable=True)
 
     logit = Add(MatMul(x, w), b)
-    return x, logit
+    return x, logit, w, b
 
 
-def train(train_x, train_y, epoches):
+def train(train_x, train_y, test_x, test_y, epoches):
+
+    x, logit, w, b = build_model(FEATURE_DIM)
     y = Variable((1, 1), init=False, trainable=False)
-
-    x, logit = build_model(FEATURE_DIM)
+    # 对logit施加Logistic函数(sigmoid)
     y_hat = Logistic(logit)
-
-    losses = LogLoss(y_hat, y)
-    optimizer = GradientDescent(default_graph, losses, 0.02, batch_size=8)
+    # 计算预测值和标签值的log loss，作为损失函数
+    loss = LogLoss(y_hat, y)
+    # 使用梯度下降优化算法
+    optimizer = GradientDescent(default_graph, loss, 0.02, batch_size=16)
     for epoch in range(epoches):
+        # 每个 epoch 开始时在测试集上评估模型正确率
+        probs = []
+        losses = []
+        for i in range(len(test_x)):
+            x.set_value(np.mat(test_x[i, :]))
+            y.set_value(np.mat(test_y[i, 0]))
+
+            # 前向传播计算概率
+            y_hat.forward()
+            probs.append(y_hat.value.A1)
+
+            # 计算损失值
+            loss.forward()
+            losses.append(loss.value.A1)
+
+        # 大于0.5的认为是正样本
+        pred = np.array([1 if x >= 0.5 else 0 for x in probs])
+        accuracy = accuracy_score(test_y.flatten(), pred)
+
+        print("Epoch: {:d}，损失值：{:.3f}，正确率：{:.2f}%".format(
+            epoch + 1, np.mean(losses), accuracy * 100))
+
         for i in range(len(train_x)):
             x.set_value(np.mat(train_x[i, :]))
             y.set_value(np.mat(train_y[i, 0]))
             optimizer.one_step()
-            print(losses.value)
+
+    # 返回训练好的模型参数
+    return w, b
 
 
-def eval():
-    pass
-
-
-FEATURE_DIM = 8
+FEATURE_DIM = 2
 if __name__ == '__main__':
-    # 构造训练数据
-    train_x, train_y, test_x, test_y = get_data(
-        2, number_of_features=FEATURE_DIM)
-    train(train_x, train_y, 5)
+    # 随机构造训练数据
+    train_x, train_y, test_x, test_y = random_gen_dateset(FEATURE_DIM, 1500)
+    w, b = train(train_x, train_y, test_x, test_y, 8)
+    plot_data(test_x[:, :2], test_y[:, 0], w.value, b.value)
--- matrixslow/ops/loss.py
+++ matrixslow/ops/loss.py
@@ -1,67 +1,33 @@
-
 # -*- coding: utf-8 -*-
 import numpy as np
 from core.node import Node
 
 
 class LogLoss(Node):
-    """
-    LogLoss函数
-    """
+    eps = 1e-15
 
     def compute(self):
+        '''
+        计算两个父节点的Log Loss
+        注意：这里有个硬性假设，第一个父节点是预测值，第二个是label
+        '''
         y_pred = self.parents[0]
         y_gt = self.parents[1]
         assert y_pred.shape() == y_gt.shape()
-        eps = 1e-15
-        prob = np.clip(y_pred.value, eps, 1 - eps)
+        prob = np.clip(y_pred.value, LogLoss.eps, 1 - LogLoss.eps)
         self.value = np.mat(np.sum(- np.multiply(y_gt.value, np.log(prob)) -
-                                   np.multiply((1 - y_gt.value), np.log(1 - prob))) / len(y_pred.value))
+                                   np.multiply((1 - y_gt.value), np.log(1 - prob))) / len(y_gt.value))
 
-    # TODO
     def get_jacobi(self, parent):
-        prob = SoftMax.softmax(self.parents[0].value)
+        '''
+        计算对父节点的雅各比矩阵
+        '''
+        y_pred = self.parents[0]
+        y_gt = self.parents[1]
+        prob = np.clip(y_pred.value, LogLoss.eps, 1 - LogLoss.eps)
+
         if parent is self.parents[0]:
-            return (prob - self.parents[1].value).T
+            return np.mat(- (y_gt.value / prob) + (1 - y_gt.value) / (1 - prob))
         else:
-            return (-np.log(prob)).T
-
-
-class SoftMax(Node):
-    """
-    SoftMax函数
-    """
-
-    @staticmethod
-    def softmax(a):
-        a[a > 1e2] = 1e2  # 防止指数过大
-        ep = np.power(np.e, a)
-        return ep / np.sum(ep)
-
-    def compute(self):
-        self.value = SoftMax.softmax(self.parents[0].value)
-
-    def get_jacobi(self, parent):
-        """
-        我们不实现SoftMax节点的get_jacobi函数，训练时使用CrossEntropyWithSoftMax节点（见下）
-        """
-        return np.mat(np.eye(self.dimension()))  # 无用
-
-
-class CrossEntropyWithSoftMax(Node):
-    """
-    对第一个父节点施加SoftMax之后，再以第二个父节点为标签One-Hot向量计算交叉熵
-    """
-
-    def compute(self):
-        prob = SoftMax.softmax(self.parents[0].value)
-        self.value = np.mat(
-            -np.sum(np.multiply(self.parents[1].value, np.log(prob + 1e-10))))
-
-    def get_jacobi(self, parent):
-        # 这里存在重复计算，但为了代码清晰简洁，舍弃进一步优化
-        prob = SoftMax.softmax(self.parents[0].value)
-        if parent is self.parents[0]:
-            return (prob - self.parents[1].value).T
-        else:
-            return (-np.log(prob)).T
+            # 默认第二个节点是label，不会参与更新，以下值无用
+            return np.mat(-np.log(prob) + np.log(1 - prob))
