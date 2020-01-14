94f7e190bc3e9411c52342bc3bd2b25f14cb4c81
zhangjuefei <zhangjuefei83@163.com>
2020-01-14 18:29:06 +0800

    不再使用优化器类。

--- adaline.py
+++ adaline.py
@@ -55,9 +55,6 @@ learning_rate = 0.0001
 # 训练执行50个epoch
 for epoch in range(50):
     
-    # 批计数器清零
-    batch_count = 0
-    
     # 遍历训练集中的样本
     for i in range(len(train_set)):
         
@@ -74,25 +71,25 @@ for epoch in range(50):
         # 在loss节点上执行前向传播，计算损失值
         loss.forward()
         
-        # 在w和b节点上执行反向传播，计算损失值对它们的雅克比矩阵
+        # 在w和b节点上执行反向传播，计算损失值对它们的雅可比矩阵
         w.backward(loss)
         b.backward(loss)
         
         """
-        用损失值对w和b的雅克比矩阵（梯度的转置）更新参数值。我们欲优化的节点
-        都应该是标量节点（才有所谓降低其值一说），它对变量节点的雅克比矩阵都
-        是1 x n的，该雅克比的转置是结果节点对变量节点的梯度。将梯度再rehape
-        成变量的形状，这样对应位置上就是结果节点对变量元素的偏导数，就可以用来
-        更新变量值了。将改变形状后的梯度乘上学习率，从当前变量值中减去，再赋值
-        给该变量节点，完成梯度下降更新。
+        用损失值对w和b的雅可比矩阵（梯度的转置）更新参数值。我们想优化的节点
+        都应该是标量节点（才有所谓降低其值一说），它对变量节点的雅可比矩阵的
+        形状都是1 x n。这个雅可比的转置是结果节点对变量节点的梯度。将梯度再
+        rehape成变量矩阵的形状，对应位置上就是结果节点对变量元素的偏导数。
+        将改变形状后的梯度乘上学习率，从当前变量值中减去，再赋值给变量节点，
+        完成梯度下降更新。
         """
         w.set_value(w.value - learning_rate * w.jacobi.T.reshape(w.shape()))
         b.set_value(b.value - learning_rate * b.jacobi.T.reshape(b.shape()))
         
-        # 计算图对象保存了所有节点，在计算图上调用clear_jacobi方法清除所有节点的雅克比矩阵
+        # default_graph对象保存了所有节点，调用clear_jacobi方法清除所有节点的雅可比矩阵
         ms.default_graph.clear_jacobi()
 
-    # 每个epoch结束后评价一下模型的正确率
+    # 每个epoch结束后评价模型的正确率
     pred = []
     
     # 遍历训练集，计算当前模型对每个样本的预测值
