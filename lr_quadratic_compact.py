da8d3b0e8cef3bfce5454bcf50a97a1d8f48864f
zhangjuefei <zhangjuefei83@163.com>
2020-02-28 18:33:43 +0800

    紧凑二次逻辑回归

--- lr_quadratic_compact.py
+++ lr_quadratic_compact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 27 17:54:09 2020
+Created on Fri Feb 28 17:28:28 2020
 
-@author: zhangjuefei
+@author: chaos
 """
 
 import numpy as np
@@ -13,8 +13,6 @@ from sklearn.datasets import make_circles
 X, y = make_circles(200, noise=0.1, factor=0.2)
 y = y * 2 - 1  # 将标签转化为1/-1
 
-# 是否使用二次项
-use_quadratic = True
 
 # 一次项，2维向量（2x1矩阵）
 x1 = ms.core.Variable(dim=(2, 1), init=False, trainable=False)
@@ -22,35 +20,23 @@ x1 = ms.core.Variable(dim=(2, 1), init=False, trainable=False)
 # 标签
 label = ms.core.Variable(dim=(1, 1), init=False, trainable=False)
 
+# 权值向量是2维（1x2矩阵）
+w = ms.core.Variable(dim=(1, 2), init=True, trainable=True)
+
+# 二次稀疏矩阵（2x2矩阵）
+W = ms.core.Variable(dim=(2, 2), init=True, trainable=True)
+
 # 偏置
 b = ms.core.Variable(dim=(1, 1), init=True, trainable=True)
 
-# 根据是否使用二次项区别处理
-if use_quadratic:
-
-    # 将一次项与自己的转置相乘，得到二次项2x2矩阵，再转成4维向量（4x1矩阵）
-    x2 = ms.ops.Reshape(
-            ms.ops.MatMul(x1, ms.ops.Reshape(x1, shape=(1, 2))),
-            shape=(4, 1)
-            )
-
-    # 将一次和二次特征连接成6维向量（6x1矩阵）
-    x = ms.ops.Concat(x1, x2)
-    
-    # 权值向量是6维（1x6矩阵）
-    w = ms.core.Variable(dim=(1, 6), init=True, trainable=True)
-    
-else:
-    
-    # 特征向量就是一次项
-    x = x1
-    
-    # 权值向量是2维（1x2矩阵）
-    w = ms.core.Variable(dim=(1, 2), init=True, trainable=True)
-
-
 # 线性部分
-output = ms.ops.Add(ms.ops.MatMul(w, x), b)
+output = ms.ops.Add(
+        ms.ops.MatMul(w, x1),   # 一次部分
+        
+        # 二次部分
+        ms.ops.MatMul(ms.ops.Reshape(x1, shape=(1, 2)),
+                      ms.ops.MatMul(W, x1)),
+        b)
 
 # 预测概率
 predict = ms.ops.Logistic(output)
