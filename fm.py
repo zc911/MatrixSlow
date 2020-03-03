789be951886fb85ffe2b23b1d420666cd39f3012
zhangjuefei <zhangjuefei83@163.com>
2020-03-03 22:20:39 +0800

    和

--- fm.py
+++ fm.py
@@ -1,30 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 28 17:28:28 2020
+Created on Tue Mar  3 16:13:19 2020
 
 @author: chaos
 """
 
 import numpy as np
-import matrixslow as ms
 from sklearn.datasets import make_circles
+import matrixslow as ms
 
-# 获取同心圆状分布的数据，X的每行包含两个特征，y是1/0类别标签
-X, y = make_circles(200, noise=0.1, factor=0.2)
-y = y * 2 - 1  # 将标签转化为1/-1
+X, y = make_circles(600, noise=0.1, factor=0.2)
+y = y * 2 - 1
 
+# 特征维数
+dimension = 20
 
-# 一次项，2维向量（2x1矩阵）
-x1 = ms.core.Variable(dim=(2, 1), init=False, trainable=False)
+# 构造噪声特征
+X = np.concatenate([X, np.random.normal(0.0, 0.5, (600, dimension-2))], axis=1)
+
+# 隐藏向量维度
+k = 2
+
+# 一次项
+x1 = ms.core.Variable(dim=(dimension, 1), init=False, trainable=False)
 
 # 标签
 label = ms.core.Variable(dim=(1, 1), init=False, trainable=False)
 
-# 权值向量是2维（1x2矩阵）
-w = ms.core.Variable(dim=(1, 2), init=True, trainable=True)
+# 一次项权值向量
+w = ms.core.Variable(dim=(1, dimension), init=True, trainable=True)
 
-# 二次项权值矩阵（2x2矩阵）
-W = ms.core.Variable(dim=(2, 2), init=True, trainable=True)
+# 隐藏向量矩阵
+H = ms.core.Variable(dim=(k, dimension), init=True, trainable=True)
+HTH = ms.ops.MatMul(ms.ops.Reshape(H, shape=(dimension, k)), H)
 
 # 偏置
 b = ms.core.Variable(dim=(1, 1), init=True, trainable=True)
@@ -34,8 +42,8 @@ output = ms.ops.Add(
         ms.ops.MatMul(w, x1),   # 一次部分
         
         # 二次部分
-        ms.ops.MatMul(ms.ops.Reshape(x1, shape=(1, 2)),
-                      ms.ops.MatMul(W, x1)),
+        ms.ops.MatMul(ms.ops.Reshape(x1, shape=(1, dimension)),
+                      ms.ops.MatMul(HTH, x1)),
         b)
 
 # 预测概率
@@ -45,16 +53,14 @@ predict = ms.ops.Logistic(output)
 loss = ms.ops.loss.LogLoss(ms.ops.Multiply(label, output))
 
 learning_rate = 0.001
-
 optimizer = ms.optimizer.Adam(ms.default_graph, loss, learning_rate)
 
 
-batch_size = 8
+batch_size = 16
 
-for epoch in range(200):
+for epoch in range(50):
     
-    batch_count = 0
-    
+    batch_count = 0   
     for i in range(len(X)):
         
         x1.set_value(np.mat(X[i]).T)
@@ -63,21 +69,21 @@ for epoch in range(200):
         optimizer.one_step()
         
         batch_count += 1
-        
         if batch_count >= batch_size:
+            
             optimizer.update()
             batch_count = 0
+        
 
     pred = []
     for i in range(len(X)):
                 
         x1.set_value(np.mat(X[i]).T)
-        label.set_value(np.mat(y[i]))
         
         predict.forward()
         pred.append(predict.value[0, 0])
             
     pred = (np.array(pred) > 0.5).astype(np.int) * 2 - 1
-    
     accuracy = (y == pred).astype(np.int).sum() / len(X)
+       
     print("epoch: {:d}, accuracy: {:.3f}".format(epoch + 1, accuracy))
