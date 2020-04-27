99dd585abbc345d9c91c468b35b1a7827dde28db
zhangjuefei <zhangjuefei83@163.com>
2020-04-27 17:01:38 +0800

    fixformat

--- matrixslow/optimizer/optimizer.py
+++ matrixslow/optimizer/optimizer.py
@@ -19,7 +19,7 @@ class Optimizer(object):
 
     def __init__(self, graph, target, learning_rate=0.01):
         """
-        优化器的构造函数接收一个计算图，计算图中的目标节点，以及学习率超参
+        优化器的构造函数接受计算图对象，目标节点对象以及学习率
         """
         assert isinstance(target, Node) and isinstance(graph, Graph)
         self.graph = graph
@@ -46,11 +46,12 @@ class Optimizer(object):
 
     @abc.abstractmethod
     def _update(self):
-        '''
+        """
         抽象方法，执行具体的梯度更新算法，由子类实现
-        '''
+        """
 
     def apply_gradients(self, node_gradients_dict, summarize=False, acc_no=None):
+
         for node, gradient in node_gradients_dict.items():
             if isinstance(node, Node):
                 pass
@@ -62,6 +63,7 @@ class Optimizer(object):
                     self.acc_gradient[target_node] += gradient
                 else:
                     self.acc_gradient[target_node] = gradient
+
         if summarize:
             self.acc_no += acc_no
         else:
@@ -72,16 +74,20 @@ class Optimizer(object):
                 self.acc_no = acc_no
 
     def update(self, var_gradients=None):
+
         if var_gradients is not None:
             self.apply_gradients(var_gradients)
+
+        # 执行更新
         self._update()
+
         # 清除累计梯度
         self.acc_gradient.clear()
         self.acc_no = 0
 
     def forward_backward(self):
         """
-        前向传播计算结果节点的值并反向传播计算结果节点对各个节点的梯度
+        前向传播计算结果节点的值并反向传播计算结果节点对各个节点的雅可比矩阵
         """
 
         # 清除计算图中所有节点的雅可比矩阵
@@ -90,13 +96,13 @@ class Optimizer(object):
         # 前向传播计算结果节点
         self.target.forward()
 
-        # 反向传播计算梯度
+        # 反向传播计算雅可比矩阵
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
                 node.backward(self.target)
 
-                # 最终结果（标量）对节点值（视作向量）的雅克比是一个行向量，将其转置是梯度（列向量）
-                # 这里将梯度 reshape 成与节点值相同的形状，好对节点值进行更新。
+                # 最终结果（标量）对节点值的雅可比是一个行向量，其转置是梯度（列向量）
+                # 这里将梯度reshape成与节点值相同的形状，好对节点值进行更新。
                 gradient = node.jacobi.T.reshape(node.shape())
                 if node not in self.acc_gradient:
                     self.acc_gradient[node] = gradient
@@ -110,68 +116,88 @@ class GradientDescent(Optimizer):
     """
 
     def __init__(self, graph, target, learning_rate=0.01):
+
         Optimizer.__init__(self, graph, target)
         self.learning_rate = learning_rate
 
     def _update(self):
         """
-        利用梯度更新可训练变量
+        朴素梯度下降法
         """
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
+
+                # 取得该节点在当前批的平均梯度
                 gradient = self.get_gradient(node)
 
+                # 用朴素梯度下降法更新变量节点的值
                 node.set_value(node.value - self.learning_rate * gradient)
 
 
 class Momentum(Optimizer):
-    '''
+    """
     Momentum动量梯度下降
-    '''
+    """
 
     def __init__(self, graph, target, learning_rate=0.01, momentum=0.9):
+
         Optimizer.__init__(self, graph, target)
+
         self.learning_rate = learning_rate
-        # 动量参数，默认为0.9
+
+        # 衰减系数，默认为0.9
         self.momentum = momentum
 
+        # 积累历史速度的字典
         self.v = dict()
 
     def _update(self):
+
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
+
+                # 取得该节点在当前批的平均梯度
                 gradient = self.get_gradient(node)
 
                 if node not in self.v:
                     self.v[node] = gradient
                 else:
-                    # 动量参数乘以历史
+                    # 滑动平均累积历史速度
                     self.v[node] = self.momentum * self.v[node] \
-                        + self.learning_rate * gradient
-                node.set_value(node.value - self.v[node])
+                        - self.learning_rate * gradient
+
+                # 更新变量节点的值
+                node.set_value(node.value + self.v[node])
 
 
 class AdaGrad(Optimizer):
-    '''
+    """
     AdaGrad优化器
-    '''
+    """
 
     def __init__(self, graph, target, learning_rate=0.01):
+
         Optimizer.__init__(self, graph, target)
+
         self.learning_rate = learning_rate
 
         self.s = dict()
 
     def _update(self):
+
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
+
+                # 取得该节点在当前批的平均梯度
                 gradient = self.get_gradient(node)
-                # 保存梯度的平方和
+
+                # 累积梯度各分量的平方和
                 if node not in self.s:
                     self.s[node] = np.power(gradient, 2)
                 else:
                     self.s[node] = self.s[node] + np.power(gradient, 2)
-                # 通过求历史平方和的开方的倒数，动态调整学习率
+
+                # 更新变量节点的值
                 node.set_value(node.value - self.learning_rate *
                                gradient / (np.sqrt(self.s[node] + 1e-10)))
 
