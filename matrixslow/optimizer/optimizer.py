189bfebcb874ad87e73757f2163be6a625829b7f
zhangjuefei <zhangjuefei83@163.com>
2020-04-27 18:18:04 +0800

    fixformat

--- matrixslow/optimizer/optimizer.py
+++ matrixslow/optimizer/optimizer.py
@@ -208,26 +208,33 @@ class RMSProp(Optimizer):
     """
 
     def __init__(self, graph, target, learning_rate=0.01, beta=0.9):
+
         Optimizer.__init__(self, graph, target)
+
         self.learning_rate = learning_rate
-        # 新的超参 beta
+
+        # 衰减系数
         assert 0.0 < beta < 1.0
         self.beta = beta
 
         self.s = dict()
 
     def _update(self):
+
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
+
+                # 取得该节点在当前批的平均梯度
                 gradient = self.get_gradient(node)
 
+                # 滑动加权累积梯度各分量的平方和
                 if node not in self.s:
                     self.s[node] = np.power(gradient, 2)
                 else:
-                    # 通过 beta 超参求加权平方
                     self.s[node] = self.beta * self.s[node] + \
                         (1 - self.beta) * np.power(gradient, 2)
-                # 与AdaGrad完全一致
+
+                # 更新变量节点的值
                 node.set_value(node.value - self.learning_rate *
                                gradient / (np.sqrt(self.s[node] + 1e-10)))
 
@@ -238,23 +245,30 @@ class Adam(Optimizer):
     """
 
     def __init__(self, graph, target, learning_rate=0.01, beta_1=0.9, beta_2=0.99):
+
         Optimizer.__init__(self, graph, target)
         self.learning_rate = learning_rate
 
+        # 历史梯度衰减系数
         assert 0.0 < beta_1 < 1.0
         self.beta_1 = beta_1
 
+        # 历史梯度各分量平方衰减系数
         assert 0.0 < beta_2 < 1.0
         self.beta_2 = beta_2
-        # 一阶梯度累积
+
+        # 历史梯度累积
         self.v = dict()
-        # 二阶梯度累积
+
+        # 历史梯度各分量平方累积
         self.s = dict()
 
     def _update(self):
 
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
+
+                # 取得该节点在当前批的平均梯度
                 gradient = self.get_gradient(node)
 
                 if node not in self.s:
@@ -264,8 +278,11 @@ class Adam(Optimizer):
                     # 梯度累积
                     self.v[node] = self.beta_1 * self.v[node] + \
                         (1 - self.beta_1) * gradient
+
+                    # 各分量平方累积
                     self.s[node] = self.beta_2 * self.s[node] + \
                         (1 - self.beta_2) * np.power(gradient, 2)
-                # 更新参数
+
+                # 更新变量节点的值
                 node.set_value(node.value - self.learning_rate *
                                self.v[node] / np.sqrt(self.s[node] + 1e-10))
