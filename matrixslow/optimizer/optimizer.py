fix initial s value for the first calculation in RMSProp and Adam class

matrixslow/optimizer/optimizer.py
@@ -272,8 +272,8 @@ class Adam(Optimizer):
                 gradient = self.get_gradient(node)
 
                 if node not in self.s:
-                    self.v[node] = gradient
-                    self.s[node] = np.power(gradient, 2)
+                    self.v[node] = (1 - self.beta_1) * gradient
+                    self.s[node] = (1 - self.beta_2) * np.power(gradient, 2)
                 else:
                     # 梯度累积
                     self.v[node] = self.beta_1 * self.v[node] + \